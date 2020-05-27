f3b78d65e2a5ff7f0ca5bb4851c718f761341619
Mark Knowles <mark@mknowles.com.au>
2020-05-27 23:14:10 +1000

    Initial Commit

---
+++ machowalk/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  machowalk
+//
+//  Created by Mark Knowles on 27/5/20.
+//  Copyright © 2020 Mark Knowles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
